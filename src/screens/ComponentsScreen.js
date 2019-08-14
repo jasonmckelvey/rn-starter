adcd431a9d79b8f7378bdbcb0c15cf84ec7d7e19
Jason <jason.mckelvey@companycam.com>
2019-08-14 10:21:05 -0500

    Updated custom JSX

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,14 +1,23 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 30
+    fontSize: 45
+  },
+  subHeader: {
+    fontSize: 20
   }
 });
 
 const ComponentsScreen = () => {
-  return <Text style={styles.textStyle}>This is the component screen</Text>;
+  const name = "Jason";
+  return (
+    <View>
+      <Text style={styles.textStyle}>Getting started with react-native</Text>
+      <Text style={styles.subHeader}>My name is {name}</Text>
+    </View>
+  );
 };
 
 export default ComponentsScreen;
