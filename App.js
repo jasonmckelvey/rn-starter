3a113f6e7d62f8616338c82537f6141b005f2bfb
Jason <jason.mckelvey@companycam.com>
2019-08-14 10:51:26 -0500

    Added ListScreen component

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from "./src/screens/ComponentsScreen";
+import ListScreeen from "./src/screens/ListScreen";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Components: ComponentsScreen
+    Components: ComponentsScreen,
+    List: ListScreeen
   },
   {
-    initialRouteName: "Components",
+    initialRouteName: "List",
     defaultNavigationOptions: {
       title: "App"
     }
