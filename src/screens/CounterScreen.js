8d099c9e7fee65bca395dea39dadbbcd0040b989
Jason <jason.mckelvey@companycam.com>
2019-08-15 10:15:44 -0500

    Added refactor to counter component using reducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import styled from "styled-components";
 import { Text, View } from "react-native";
 import Button from "../components/Button";
@@ -6,7 +6,6 @@ import Button from "../components/Button";
 const Wrapper = styled.View`
   align-items: center;
   padding: 8px;
-  /* flex: 1; */
 `;
 const InfoText = styled.Text`
   margin: 16px;
@@ -14,23 +13,33 @@ const InfoText = styled.Text`
   font-weight: 600;
 `;
 
+const reducer = (state, action) => {
+  // state === { count: number }
+  // action === { type: 'increment' || 'decrement', payload: 1 }
+
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + action.payload };
+    case "decrement":
+      return { ...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
+
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
   return (
     <Wrapper style={{ flex: 1 }}>
       <Button
         title="Increase"
-        onPress={() => {
-          setCounter(counter + 1);
-        }}
+        onPress={() => dispatch({ type: "increment", payload: 1 })}
       />
       <Button
         title="Decrease"
-        onPress={() => {
-          setCounter(counter - 1);
-        }}
+        onPress={() => dispatch({ type: "decrement", payload: 1 })}
       />
-      <InfoText>Current Count: {counter}</InfoText>
+      <InfoText>Current Count: {state.count}</InfoText>
     </Wrapper>
   );
 };
